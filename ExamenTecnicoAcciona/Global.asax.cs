fcfdb54c981c2c77a8ceaedb178767e0682f1971
Carlos Quiroz <CRamirez@sofrecom.com.ar>
2020-04-13 13:12:11 -0300

    Creacion de repositorio

---
+++ ExamenTecnicoAcciona/Global.asax.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Web.Http;
+using ExamenTecnicoAcciona.Configuration;
+using System.Web.Routing;
+[assembly: log4net.Config.XmlConfigurator(Watch =true)]
+
+namespace ExamenTecnicoAcciona
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            UnityConfig.RegisterComponents();
+            /*
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);*/
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
